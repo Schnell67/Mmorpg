a7ad8c358452c061e5db32fa158f178059397294
Schnell67 <lucasschnell32@gmail.com>
2020-12-14 10:15:03 +0100

    v1

--- src/Guerrier.java
+++ src/Guerrier.java
@@ -1,15 +1,11 @@
 
 public class Guerrier extends Personnage {
 
-	public Guerrier(String ps, int niv) {
+	public Guerrier(String ps, int niv, int intiatiative) {
 		super(ps, niv);
+		super.pointdevie = super.niveau *8 + 4;
+		super.initiative = super.niveau * 4 + 6;
 		// TODO Auto-generated constructor stub
-	}
-	
-	public Guerrier() {
-		super();
-		
-		
 	}
 
 	
