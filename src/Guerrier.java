0b3c97b6800500fbf853a357a1afad449b35d696
Schnell67 <lucasschnell32@gmail.com>
2020-12-14 10:29:11 +0100

    v1

--- src/Guerrier.java
+++ src/Guerrier.java
@@ -1,12 +1,19 @@
-
 public class Guerrier extends Personnage {
-
-	public Guerrier(String ps, int niv, int intiatiative) {
-		super(ps, niv);
-		super.pointdevie = super.niveau *8 + 4;
-		super.initiative = super.niveau * 4 + 6;
-		// TODO Auto-generated constructor stub
-	}
-
-	
+    String  monture;
+    public Guerrier (String ps) {
+        // appel au constructeur associé de Personnage. Cela doit toujours être en première ligne
+        super(ps);
+        // modification des attributs hérité de Personnage
+        super.pointdevie = super.niveau * 8 + 4;
+        super.initiative = super.niveau * 4 + 6;
+        this.monture = "";
+    }
+    
+    public Guerrier(String ps, int n, String m) {
+        super(ps,n); // equivalent à new Personnage (ps, n);
+        super.pointdevie = super.niveau * 8 + 4;
+        super.initiative = super.niveau * 4 + 6;
+        
+        this.monture = m;
+    }
 }
